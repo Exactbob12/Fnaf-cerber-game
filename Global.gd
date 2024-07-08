0cf7749645128666848d6a8a0154684831a9b41c
Walmart Wan <git config --global user.email afanfromwalmart@gmail.comgit config --global user.email>
2024-07-07 17:28:42 -0700

    Got a tiny bit more done(I mean tiny)

--- Global.gd
+++ Global.gd
@@ -7,7 +7,8 @@ var scaleX:float
 var scaleY:float
 var originalwidth = 1152
 var originalheight = 648
+var CharPositons = [0,1,0,0,2]
+var currentcam:int = -1
 func _process(delta):
 	scaleX = float(DisplayServer.window_get_size().x / originalwidth)
 	scaleY = float(DisplayServer.window_get_size().y / originalheight)
-	print(scaleX," ",scaleY)
--- title text.gd
+++ title text.gd
@@ -2,7 +2,7 @@ extends Label
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	label_settings.font_size = 76*Global.scaleX
+	label_settings.font_size = 76 * pow(minf(Global.scaleX,Global.scaleY) , 0.34)
 	if Global.scene == 0:
 		self.visible = true
 	else:
